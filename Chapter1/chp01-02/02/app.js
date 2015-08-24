d2bc45523ed4e8074a4d7d66a295846babf62615
ryota-murakami <dojce1048@gmail.com>
2015-08-24 14:40:13 +0900

    fix mistake chapter

--- Chapter1/chp01-02/02/app.js
+++ Chapter1/chp01-02/02/app.js
@@ -1,111 +1,42 @@
-function AppModel(attrs) {
-  this.val = "";
-  this.attrs = {
-    required: attrs.required || false,
-    maxlength: attrs.maxlength || 8,
-    minlength: attrs.minlength || 4
-  };
-  this.listeners = {
-    valid: [],
-    invalid: []
-  };
+function Observer() {
+  this.listeners = {};
 }
 
-AppModel.prototype.set = function(val) {
-  if (this.val === val) return;
-  this.val = val;
-  this.validate();
-};
-
-AppModel.prototype.validate = function() {
-  var val;
-  this.errors = [];
-
-  for (var key in this.attrs) {
-    val = this.attrs[key];
-    if (val && !this[key](val)) this.errors.push(key);
+Observer.prototype.on = function(event, func) {
+  if (! this.listeners[event] ) {
+    this.listeners[event] = [];
   }
-
-  this.trigger(!this.errors.length ? "valid" : "invalid");
-};
-
-AppModel.prototype.on = function(event, func) {
   this.listeners[event].push(func);
 };
 
-AppModel.prototype.trigger = function(event) {
-  $.each(this.listeners[event], function() {
-    this();
-  });
-};
-
-AppModel.prototype.required = function() {
-  return this.val !== "";
-};
-
-AppModel.prototype.maxlength = function(num) {
-  return num >= this.val.length;
-};
-
-AppModel.prototype.minlength = function(num) {
-  return num <= this.val.length;
-};
-
-function AppView() {
-  this.initialize.apply(this, arguments);
-  this.handleEvents();
-}
-
-AppView.prototype.initialize = function(el) {
-  this.$el = $(el);
-  this.$list = this.$el.next().children();
-
-  var obj = this.$el.data();
-
-  if (this.$el.prop("required")) {
-    obj["required"] = true;
+Observer.prototype.off = function(event, func) {
+  var ref = this.listeners[event],
+      len = ref.length;
+  for (var i = 0; i < len; i++) {
+    var listener = ref[i];
+    if (listener === func) {
+      ref.splice(i, 1);
+    }
   }
-
-  this.model = new AppModel(obj);
 };
 
-AppView.prototype.handleEvents = function() {
-  var self = this;
-
-  this.$el.on("keyup", function(e) {
-    self.onKeyup(e);
-  });
-
-  this.model.on("valid", function() {
-    self.onValid();
-  });
-
-  this.model.on("invalid", function() {
-    self.onInvalid();
-  });
-
+Observer.prototype.trigger = function(event) {
+  var ref = this.listeners[event];
+  for (var i = 0, len = ref.length; i < len; i++) {
+    var listener = ref[i];
+    if(typeof listener === "function") listener();
+  }
 };
 
-AppView.prototype.onKeyup = function(e) {
-  var $target = $(e.currentTarget);
-  this.model.set($target.val());
+var observer = new Observer();
+var greet = function () {
+  console.log("Good morning");
 };
+observer.on("morning", greet);
+observer.trigger("morning"); //Good morning
 
-AppView.prototype.onValid = function() {
-  this.$el.removeClass("error");
-  this.$list.hide();
+var sayEvening = function () {
+console.log("Good evening");
 };
-
-AppView.prototype.onInvalid = function() {
-  var self = this;
-  this.$el.addClass("error");
-  this.$list.hide();
-
-  $.each(this.model.errors, function(index, val) {
-    self.$list.filter("[data-error=\"" + val + "\"]").show();
-  });
-};
-
-$("input").each(function() {
-  new AppView(this);
-});
+observer.on("evening", sayEvening);
+observer.trigger("evening"); // Good evening

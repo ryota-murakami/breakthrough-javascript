ce516a1ad4c8cd12df118b27816be64962897166
ryota-murakami <dojce1048@gmail.com>
2015-08-24 13:17:32 +0900

    complete chp01-02/02/app.jp

--- Chapter1/chp01-02/02/app.js
+++ Chapter1/chp01-02/02/app.js
@@ -3,9 +3,9 @@ function AppModel(attrs) {
   this.attrs = {
     required: attrs.required || false,
     maxlength: attrs.maxlength || 8,
-    minLength: attrs.minLength || 4
+    minlength: attrs.minlength || 4
   };
-  this.listenrs = {
+  this.listeners = {
     valid: [],
     invalid: []
   };
@@ -63,6 +63,49 @@ AppView.prototype.initialize = function(el) {
   var obj = this.$el.data();
 
   if (this.$el.prop("required")) {
-    obj["required"] = ture;
+    obj["required"] = true;
   }
-}
+
+  this.model = new AppModel(obj);
+};
+
+AppView.prototype.handleEvents = function() {
+  var self = this;
+
+  this.$el.on("keyup", function(e) {
+    self.onKeyup(e);
+  });
+
+  this.model.on("valid", function() {
+    self.onValid();
+  });
+
+  this.model.on("invalid", function() {
+    self.onInvalid();
+  });
+
+};
+
+AppView.prototype.onKeyup = function(e) {
+  var $target = $(e.currentTarget);
+  this.model.set($target.val());
+};
+
+AppView.prototype.onValid = function() {
+  this.$el.removeClass("error");
+  this.$list.hide();
+};
+
+AppView.prototype.onInvalid = function() {
+  var self = this;
+  this.$el.addClass("error");
+  this.$list.hide();
+
+  $.each(this.model.errors, function(index, val) {
+    self.$list.filter("[data-error=\"" + val + "\"]").show();
+  });
+};
+
+$("input").each(function() {
+  new AppView(this);
+});
